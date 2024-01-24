ca3835f299d4be64af757d0b2c1867cf467009fc
alhammanazil <alhammanazil18@gmail.com>
2024-01-24 14:02:05 +0700

    install module in vuejs

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "animate.css/animate.min.css";
+import {Library, library} from "@fortawesome/fontawesome-svg-core";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faShoppingCart);
+
+Vue.config.productinTip = false;
 
 createApp(App).mount("#app");
