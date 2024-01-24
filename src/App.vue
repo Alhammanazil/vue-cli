35a7bd149bdae1314789bea60ced8333a32f8a0f
alhammanazil <alhammanazil18@gmail.com>
2024-01-24 14:31:29 +0700

    module implementation in vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" class="container mt-5">
+    <h1>IDShop</h1>
+    <p class="animated fadeInRight">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas aliquam
+      quaerat ut molestiae dignissimos dolorum repellendus, cum et asperiores
+      nostrum harum blanditiis dolores, quibusdam distinctio! Dolore, assumenda
+      recusandae! Corrupti, dicta.
+    </p>
+    <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faShoppingCart);
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    FontAwesomeIcon,
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "animate.css/animate.min.css";
-import {Library, library} from "@fortawesome/fontawesome-svg-core";
+import "bootstrap/dist/css/bootstrap.css";
+import "animate.css/animate.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 library.add(faShoppingCart);
-
-Vue.config.productinTip = false;
 
 createApp(App).mount("#app");
